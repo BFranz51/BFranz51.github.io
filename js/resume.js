3396627b63b9379556ccc02c53cb8f18b3a15d0e
BFranz51 <bfranz@mail.usf.edu>
2018-03-11 14:36:54 -0400

    Changed <br> to <br /> for XHTML compatibility. Commented JS scroll function and increased speed.

--- js/resume.js
+++ js/resume.js
@@ -2,14 +2,19 @@
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  //$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href^="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Get object with id/name = URL (after #)
       var target = $(this.hash);
+      // If no object with correct id, search for name = URL
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Final check to ensure we have a target
       if (target.length) {
+        // Cross browser scroll animation
         $('html, body').animate({
           scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
+        }, {easing:'easeInOutExpo', duration:800 });
         return false;
       }
     }
